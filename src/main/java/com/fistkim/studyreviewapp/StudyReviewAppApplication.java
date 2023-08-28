f0095593a4790b17cde653ff2cbcd83922a4486b
fistkim101 <fistkim101@gmail.com>
2023-08-28 12:22:47 +0900

    project init

---
+++ src/main/java/com/fistkim/studyreviewapp/StudyReviewAppApplication.java
@@ -0,0 +1,13 @@
+package com.fistkim.studyreviewapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyReviewAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyReviewAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fistkim/studyreviewapp/StudyReviewAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fistkim.studyreviewapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyReviewAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
